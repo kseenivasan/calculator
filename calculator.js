5bc848821e9d26911d161583cc3ac0f2b2bd99b0
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-22 18:56:23 +0530

    Add further functionality to calculator

--- calculator.js
+++ calculator.js
@@ -26,7 +26,65 @@ const operate = function(operation,a,b) {
             return a*b;
             break;
         case "/":
+            if (b === 0) {
+                return "cannot divide by 0!"
+            }
             return a/b;
             break;
     }
 }
+
+let displayValue = "";
+let displayField = document.querySelector(".display");
+
+let numButtons = document.querySelectorAll(".number");
+
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        displayValue += button.textContent;
+        displayField.textContent = displayValue;
+    });
+});
+let operand;
+let firstNum = undefined;
+let secondNum = undefined;
+let secondNumStarting;
+let operationButtons = document.querySelectorAll(".operand");
+operationButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        operand = button.textContent;
+        if (firstNum === undefined) {
+            firstNum = Number(displayValue);
+        }
+        else {
+            secondNum = Number(displayValue.substring(secondNumStarting));
+            solution = operate(operand,firstNum,secondNum);
+            firstNum = solution;
+        }
+        secondNumStarting = displayValue.length + 1;
+        displayValue += button.textContent;
+        displayField.textContent = displayValue;
+        
+    })
+})
+
+let equalButton = document.querySelector(".equals");
+let solution;
+equalButton.addEventListener("click",() => {
+    secondNum = Number(displayValue.substring(secondNumStarting));
+    solution = operate(operand,firstNum,secondNum);
+    displayField.textContent = solution;
+    displayValue = "";
+    firstNum = undefined;
+    secondNum = undefined;
+})
+
+let clearButton = document.querySelector(".clear");
+
+clearButton.addEventListener("click", () => {
+    displayValue = "";
+    displayField.textContent = displayValue;
+    firstNum = undefined;
+    secondNum = undefined;
+    solution = undefined;
+})
